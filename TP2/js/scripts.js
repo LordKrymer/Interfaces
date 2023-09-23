03bf3299d0d19c668db2d31de020d7863092f4ce
Lulo <lulobareiss@gmail.com>
2023-09-23 18:17:08 -0300

    footer fixed, js spa

---
+++ TP2/js/scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded',function(){inicializar()})
+console.log("el que lee esto es puto");
+
+function inicializar(){
+    fetchArchivo("home.html");
+    rutaUrl("home.html"); 
+}
+
+async function fetchArchivo(ruta){ 
+    let promise = await fetch("html/"+ruta);
+    let contenedor = document.getElementById("index");
+    let contenido  = await promise.text();
+    contenedor.innerHTML = contenido;
+}
+
+function rutaUrl(ruta){// muestra la ruta url
+    history.pushState({page:ruta},"index","/"+ruta);
+}
+
+window.addEventListener('popstate', function(){
+    fetchArchivo(history.state.page);
+    inicializarContacto(this.history.state.page);
+});
+
+
