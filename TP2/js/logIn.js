d841ed209f79af1a0ae3c15f3bf7abedb1d63752
Lulo <lulobareiss@gmail.com>
2023-10-09 12:39:16 -0300

    code refactor

--- TP2/js/logIn.js
+++ TP2/js/logIn.js
@@ -1,8 +1,10 @@
 
 let loginButton = document.getElementById("login");
+
 loginButton.addEventListener("click",function(e){
     e.preventDefault();
 });
+
 loginButton.addEventListener("click",()=>{
     changeRoute("home")
 });
