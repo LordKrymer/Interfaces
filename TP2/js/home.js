bbfc7a00ff6ae86cd564e83d98909022c2c4406f
Lulo <lulobareiss@gmail.com>
2023-10-18 18:00:23 -0300

    dropdown menu implemented, and code refactor

--- TP2/js/home.js
+++ TP2/js/home.js
@@ -18,14 +18,4 @@ document.getElementById("arrow-right").addEventListener('click',(e)=>{
     carousel.style.transition = `1s ease`
     carousel.style.transform = `translateX(-${Math.min(Number(carousel.dataset.scroll) + 400,maxX)}px)`;
     carousel.dataset.scroll = Math.min(Number(carousel.dataset.scroll) + 400,maxX)
-
-    
-    console.log(carousel)
-    //carousel.scroll({left:Number(carousel.dataset.scroll)+400,behavior:"smooth"})
-    //carousel.dataset.scroll= Number(carousel.dataset.scroll)+400
 })
-//document.getElementById("arrow-right").addEventListener('click',(e)=>{e.target.parentElement.previousElementSibling.firstElementChild.scroll({left:400,behavior:"smooth"})})
-
-
-//document.querySelector("#arrow-right").addEventListener('click',(e)=>{console.log(e.target.parentElement.previousElementSibling)})
-
--- TP2/js/router.js
+++ TP2/js/router.js
@@ -43,4 +43,20 @@ window.addEventListener('popstate', function(){
     fetchFile(history.state.page);
 });
 
-
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.hamburger-button')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
