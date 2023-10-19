ddc7e950ac6caddf50f611224588f06ae133af77
Lulo <lulobareiss@gmail.com>
2023-10-19 11:43:41 -0300

    code refactor

--- TP2/js/router.js
+++ TP2/js/router.js
@@ -43,20 +43,21 @@ window.addEventListener('popstate', function(){
     fetchFile(history.state.page);
 });
 
-function myFunction() {
+document.querySelector('.hamburger-button').addEventListener('click', deployMenu);
+
+function deployMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.hamburger-button')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.hamburger-button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
